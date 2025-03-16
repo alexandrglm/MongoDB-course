65d40b2716e742bcade525bc5f10f2ffbb549825
Alexandr Gomez <alexandrglm@users.noreply.github.com>
2025-03-16 17:54:40 +0100

    Started MongoDb consulting/editing framework defyining Flask app.routes

--- mongo.py
+++ mongo.py
@@ -11,13 +11,44 @@
 # - PyMongo
 # - MongoEngine
 #################################
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, send_from_directory, jsonify, request
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 #################################
 app = Flask(__name__)
-#################################
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def statics(filename):
+
+    return send_from_directory('static', filename)
+#################################
+# MongoDB back-end setups
+client = MongoClient('mongodb://localhost:27017')
+db = client['db']
+
+# Collections
+books_collection = db['books']
+users_collections = db['users']
+#################################
+# Endpoints routes
+## POST endpoint
+# @app.route('/books')
+
+
+## GET ALL endpoint
+# @app.route('/books')
+
+## GET endpoint
+# @app.route('/books/<book_id>', methods=['GET'])
+# def get_book():
+#    book = books_collection
+
+## DELETE endpoint
+#@app.route('/books/<book_id>')
 
 
 
